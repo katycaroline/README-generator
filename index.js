d1311962656cad09ba152f3e6a1754b2bcf89e69
Katy Thompson <katimation@gmail.com>
2023-11-24 13:45:23 -0600

    added questions and writeToFile function

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How can users install your project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How can your project be used?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for this project.",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Apache 2.0",
+            "Boost Software 1.0",
+            "MIT",
+            "Mozilla",
+        ],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can people contribute to the project?",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
